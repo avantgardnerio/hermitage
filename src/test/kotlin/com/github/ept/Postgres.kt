4402f17d8c228ff8bf0958cbf99d0fbe2f51c13e
Brent Gardner <brent.gardner@spaceandtime.io>
2023-03-11 12:35:57 -0700

    otv

--- src/test/kotlin/com/github/ept/Postgres.kt
+++ src/test/kotlin/com/github/ept/Postgres.kt
@@ -11,10 +11,8 @@ import kotlin.test.assertFalse
 import kotlin.test.assertTrue
 
 class Postgres {
-    val con1: Connection
-    val con2: Connection
-    val stmt1: Statement
-    val stmt2: Statement
+    val cons: Array<Connection>
+    val stmts: Array<Statement>
 
     init {
         val driver = org.postgresql.Driver()
@@ -22,31 +20,133 @@ class Postgres {
         val props = Properties()
         props.setProperty("user", "postgres")
         props.setProperty("password", "")
-        con1 = driver.connect(url, props)!!
-        con2 = driver.connect(url, props)!!
-        stmt1 = con1.createStatement()
-        stmt2 = con2.createStatement()
+        cons = (0 until 3).map { driver.connect(url, props)!! }.toTypedArray()
+        stmts = cons.map { it.createStatement() }.toTypedArray()
     }
 
-    fun execute(sql: String) {
-        val re = """--\s*(\w+)""".toRegex()
-        val stmtIdx = re.find(sql)!!.groups[1]!!.value
-        val stmts = when (stmtIdx) {
-            "T1" -> arrayOf(stmt1)
-            "T2" -> arrayOf(stmt2)
-            "either" -> arrayOf(stmt1, stmt2)
-            else -> throw Exception("Invalid transaction number!")
+    @BeforeEach
+    fun setup() {
+        stmts[0].execute("drop table if exists test")
+        stmts[0].execute("create table test (id int primary key, value int)")
+        stmts[0].executeUpdate("insert into test (id, value) values (1, 10), (2, 20)")
+    }
+
+    @Test
+    fun g0() {
+        val wasCalled = AtomicBoolean(false)
+        execute("begin; set transaction isolation level read committed; -- T1")
+        execute("begin; set transaction isolation level read committed; -- T2")
+        execute("update test set value = 11 where id = 1; -- T1")
+
+        val t2 = Thread {
+            execute("update test set value = 12 where id = 1; -- T2, BLOCKS")
+            assertTrue(wasCalled.get(), "t1 should have committed before t2 update complete!")
         }
+        t2.start()
+        execute("update test set value = 21 where id = 2; -- T1")
+        Thread.sleep(500)
+        assertFalse(wasCalled.getAndSet(true), "t2 should not have updated until t1 commits!")
+        execute("commit; -- T1. This unblocks T2")
+        t2.join()
+
+        assertQuery("select * from test; -- T1. Shows 1 => 11, 2 => 21")
+        execute("update test set value = 22 where id = 2; -- T2")
+        execute("commit; -- T2")
+        assertQuery("select * from test; -- either. Shows 1 => 12, 2 => 22")
+    }
+
+    @Test
+    fun otv() {
+        val wasCalled = AtomicBoolean(false)
+        execute("begin; set transaction isolation level read committed; -- T1")
+        execute("begin; set transaction isolation level read committed; -- T2")
+        execute("begin; set transaction isolation level read committed; -- T3")
+        execute("update test set value = 11 where id = 1; -- T1")
+        execute("update test set value = 19 where id = 2; -- T1")
+
+        val t2 = Thread {
+            execute("update test set value = 12 where id = 1; -- T2. BLOCKS")
+            assertTrue(wasCalled.get(), "t1 should have committed before t2 update complete!")
+        }
+        t2.start()
+        Thread.sleep(500)
+        assertFalse(wasCalled.getAndSet(true), "t2 should not have updated until t1 commits!")
+        execute("commit; -- T1. This unblocks T2")
+        t2.join()
+
+        assertQuery("select * from test where id = 1; -- T3. Shows 1 => 11")
+        execute("update test set value = 18 where id = 2; -- T2")
+        assertQuery("select * from test where id = 2; -- T3. Shows 2 => 19")
+        execute("commit; -- T2")
+        assertQuery("select * from test where id = 2; -- T3. Shows 2 => 18")
+        assertQuery("select * from test where id = 1; -- T3. Shows 1 => 12")
+        execute("commit; -- T3")
+    }
+
+    @Test
+    fun g1a() {
+        execute("begin; set transaction isolation level read committed; -- T1")
+        execute("begin; set transaction isolation level read committed; -- T2")
+        execute("update test set value = 101 where id = 1; -- T1")
+        assertQuery("select * from test; -- T2. Still shows 1 => 10")
+        execute("abort;  -- T1")
+        assertQuery("select * from test; -- T2. Still shows 1 => 10")
+        execute("commit; -- T2")
+    }
+
+    @Test
+    fun g1b() {
+        execute("begin; set transaction isolation level read committed; -- T1")
+        execute("begin; set transaction isolation level read committed; -- T2")
+        execute("update test set value = 101 where id = 1; -- T1")
+        assertQuery("select * from test; -- T2. Still shows 1 => 10")
+        execute("update test set value = 11 where id = 1; -- T1")
+        execute("commit; -- T1")
+        assertQuery("select * from test; -- T2. Now shows 1 => 11")
+        execute("commit; -- T2")
+    }
+
+    @Test
+    fun g1c() {
+        execute("begin; set transaction isolation level read committed; -- T1")
+        execute("begin; set transaction isolation level read committed; -- T2")
+        execute("update test set value = 11 where id = 1; -- T1")
+        execute("update test set value = 22 where id = 2; -- T2")
+        assertQuery("select * from test where id = 2; -- T1. Still shows 2 => 20")
+        assertQuery("select * from test where id = 1; -- T2. Still shows 1 => 10")
+        execute("commit; -- T1")
+        execute("commit; -- T2")
+    }
+
+    // --- helpers
+    fun execute(sql: String) {
+        val stmts = getStatements(sql)
         stmts.forEach { stmt ->
             stmt.execute(sql)
         }
     }
 
-    fun assertQuery(stmt: Statement, sql: String) {
+    fun getStatements(sql: String): Array<Statement> {
+        val re = """--\s*(\w+)""".toRegex()
+        val stmtIdx = re.find(sql)!!.groups[1]!!.value
+        val stmts = when (stmtIdx) {
+            "T1" -> arrayOf(stmts[0])
+            "T2" -> arrayOf(stmts[1])
+            "T3" -> arrayOf(stmts[2])
+            "either" -> arrayOf(stmts[0], stmts[1])
+            else -> throw Exception("Invalid transaction number!")
+        }
+        return stmts
+    }
+
+    fun assertQuery(sql: String) {
         val expected = commentToMap(sql)
-        val actual = queryToMap(stmt, sql)
-        val compare = actual.filterKeys { expected.containsKey(it) }
-        assertEquals(expected, compare)
+        val stmts = getStatements(sql)
+        stmts.forEach { stmt ->
+            val actual = queryToMap(stmt, sql)
+            val compare = actual.filterKeys { expected.containsKey(it) }
+            assertEquals(expected, compare)
+        }
     }
 
     fun queryToMap(stmt: Statement, sql: String): Map<Int, Int> {
@@ -75,69 +175,4 @@ class Postgres {
         return res
     }
 
-    @BeforeEach
-    fun setup() {
-        stmt1.execute("drop table if exists test")
-        stmt1.execute("create table test (id int primary key, value int)")
-        stmt1.executeUpdate("insert into test (id, value) values (1, 10), (2, 20)")
-    }
-
-    @Test
-    fun g0() {
-        val wasCalled = AtomicBoolean(false)
-        execute("begin; set transaction isolation level read committed; -- T1")
-        execute("begin; set transaction isolation level read committed; -- T2")
-        execute("update test set value = 11 where id = 1; -- T1")
-        val t2 = Thread {
-            execute("update test set value = 12 where id = 1; -- T2, BLOCKS")
-            assertTrue(wasCalled.get(), "t1 should have committed before t2 update complete!")
-        }
-        t2.start()
-        execute("update test set value = 21 where id = 2; -- T1")
-        Thread.sleep(500)
-        assertFalse(wasCalled.getAndSet(true), "t2 should not have updated until t1 commits!")
-        execute("commit; -- T1. This unblocks T2")
-        t2.join()
-        assertQuery(stmt1, "select * from test; -- T1. Shows 1 => 11, 2 => 21")
-        execute("update test set value = 22 where id = 2; -- T2")
-        execute("commit; -- T2")
-        arrayOf(stmt1, stmt2).forEach { stmt ->
-            assertQuery(stmt, "select * from test; -- either. Shows 1 => 12, 2 => 22")
-        }
-    }
-
-    @Test
-    fun g1a() {
-        execute("begin; set transaction isolation level read committed; -- T1")
-        execute("begin; set transaction isolation level read committed; -- T2")
-        execute("update test set value = 101 where id = 1; -- T1")
-        assertQuery(stmt2, "select * from test; -- T2. Still shows 1 => 10")
-        execute("abort;  -- T1")
-        assertQuery(stmt2, "select * from test; -- T2. Still shows 1 => 10")
-        execute("commit; -- T2")
-    }
-
-    @Test
-    fun g1b() {
-        execute("begin; set transaction isolation level read committed; -- T1")
-        execute("begin; set transaction isolation level read committed; -- T2")
-        execute("update test set value = 101 where id = 1; -- T1")
-        assertQuery(stmt2, "select * from test; -- T2. Still shows 1 => 10")
-        execute("update test set value = 11 where id = 1; -- T1")
-        execute("commit; -- T1")
-        assertQuery(stmt2, "select * from test; -- T2. Now shows 1 => 11")
-        execute("commit; -- T2")
-    }
-
-    @Test
-    fun g1c() {
-        execute("begin; set transaction isolation level read committed; -- T1")
-        execute("begin; set transaction isolation level read committed; -- T2")
-        execute("update test set value = 11 where id = 1; -- T1")
-        execute("update test set value = 22 where id = 2; -- T2")
-        assertQuery(stmt1, "select * from test where id = 2; -- T1. Still shows 2 => 20")
-        assertQuery(stmt2, "select * from test where id = 1; -- T2. Still shows 1 => 10")
-        execute("commit; -- T1")
-        execute("commit; -- T2")
-    }
 }
